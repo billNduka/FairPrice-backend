b1f65ca01cee229221bcd793a4e65d46a4617b03
billNduka <billnduka01@gmail.com>
2025-10-01 23:44:16 +0100

    Added actual scraping for single page

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    async searchProducts(query:string = "Tecno Camon 40"){
+        let results: { title: string | undefined; price: string; link: string }[] = [];
+        const url = `https://www.jumia.com.ng/catalog/?q=${encodeURIComponent(query)}`;
+
+        try{
+            const { data } = await axios.get(url);  
+            const $ = cheerio.load(data);
+
+            $('a.core').each((i, el) => {
+                const title = $(el).find('.name').text();
+                const price = $(el).find('.prc').text();
+                const link = "https://www.jumia.com.ng" + $(el).attr('href');
+
+                if (title && price) {
+                    results.push({ title, price, link });
+                }
+
+            });
+
+            return {
+                    query,
+                    count: results.length,
+                    lowestPrice: results.length ? Math.min(...results.map(r => parseInt(r.price.replace(/\D/g, "")))) : null,
+                    highestPrice: results.length ? Math.max(...results.map(r => parseInt(r.price.replace(/\D/g, "")))) : null,
+                    results
+            };
+        }catch(error){
+            console.error("Error scraping: ", query, error.message)
+        }
+    }
+}
+
